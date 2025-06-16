e3cfb653a7ea5f9309c4a172a90f070c27ff59a1
FranchyR <franchyrech@gmail.com>
2025-06-15 22:39:04 -0300

    Adicionado schema User

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -2,11 +2,23 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fastapi_zero.schemas import Message
+from fastapi_zero.schemas import Message, UserDB, UserPublic, UserSchema
 
 app = FastAPI()
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá mundo!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(
+        username = user.username,
+        email = user.email,
+        password = user.password,
+        id=len(database) + 1,
+    )
+    return user_with_id
