dd4ae240f45425a5a29e9fb6ffeb2124e6086332
FranchyR <franchyrech@gmail.com>
2025-06-23 21:36:03 -0300

    Refatorando estrutura do projeto: Suporte a asyncio, tornando o projeto não bloqueante

--- fastapi_zero/routers/users.py
+++ fastapi_zero/routers/users.py
@@ -4,7 +4,7 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from fastapi_zero.database import get_session
 from fastapi_zero.models import User
@@ -21,13 +21,13 @@ from fastapi_zero.security import (
 )
 
 router = APIRouter(prefix='/users', tags=['users'])
-Session = Annotated[Session, Depends(get_session)]
+Session = Annotated[AsyncSession, Depends(get_session)]
 CurrentUser = Annotated[User, Depends(get_current_user)]
 
 
 @router.post('/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
-def create_user(user: UserSchema, session: Session):
-    db_user = session.scalar(
+async def create_user(user: UserSchema, session: Session):  # type: ignore
+    db_user = await session.scalar(
         select(User).where(
             (User.username == user.username) | (User.email == user.email)
         )
@@ -54,21 +54,20 @@ def create_user(user: UserSchema, session: Session):
     )
 
     session.add(db_user)
-    session.commit()
-    session.refresh(db_user)
+    await session.commit()
+    await session.refresh(db_user)
 
     return db_user
 
 
 @router.get('/', status_code=HTTPStatus.OK, response_model=UserList)
-def read_users(
+async def read_users(
     session: Session,
     current_user: CurrentUser,
     filter_users: Annotated[FilterPage, Query()],
 ):
-    users = session.scalars(
-        select(User).offset(filter_users.offset).limit(filter_users.limit)
-    ).all()
+    users = await session.scalars(
+        select(User).offset(filter_users.offset).limit(filter_users.limit))
 
     return {'users': users}
 
@@ -77,7 +76,7 @@ def read_users(
 def update_user(
     user_id: int,
     user: UserSchema,
-    session: Session,
+    session: Session,  # type: ignore
     current_user: CurrentUser,
 ):
     if current_user.id != user_id:
@@ -105,7 +104,7 @@ def update_user(
 @router.delete('/{user_id}', status_code=HTTPStatus.OK, response_model=Message)
 def delete_user(
     user_id: int,
-    session: Session,
+    session: Session,  # type: ignore
     current_user: CurrentUser,
 ):
     if current_user.id != user_id:
